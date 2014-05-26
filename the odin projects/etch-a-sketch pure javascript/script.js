57afce0273044da327af37c980a04903ee100832
Emnalyeriar <emnalyeriar@gmail.com>
2014-05-26 22:50:06 +0200

    Added few drawing features: black, color, fading and slowly changing black to white. It was a bit hard to do the last one because I wanted to use pure JavaScript only without the use of jQuery but I finally done it.

--- the odin projects/etch-a-sketch pure javascript/script.js
+++ the odin projects/etch-a-sketch pure javascript/script.js
@@ -20,13 +20,48 @@
                     this.style.backgroundColor="black";
                 };
             }        
+        },
+        random = function () {
+            return Math.floor(Math.random() * 256);
+        },
+        color = function() {
+            var divs = main[0].getElementsByTagName("div");
+            for (var i=0, l=divs.length; i < l; i++) {
+                divs[i].onmouseover = function() {
+                    this.style.backgroundColor="rgb("+random()+","+random()+","+random()+")";
+                };
+            }        
+        },
+        fading = function() {
+            var divs = main[0].getElementsByTagName("div");
+            for (var i=0, l=divs.length; i < l; i++) {
+                divs[i].onmouseover = function() {
+                    this.style.backgroundColor="black";
+                    var that = this;
+                    setTimeout(function() {
+                        that.style.backgroundColor="#d9d9d9";
+                    }, 200);
+                };
+            }               
+        },
+        slowly = function() {
+            var divs = main[0].getElementsByTagName("div");
+            for (var i=0, l=divs.length; i < l; i++) {
+                divs[i].onmouseover = function() {
+                    this.style.backgroundColor="black";
+                    opacity =  parseFloat(window.getComputedStyle(this, null).getPropertyValue("opacity"));
+                    this.style.opacity = opacity - .1;
+                };
+            };
         };
     document.getElementById("size").onclick = function() {
         divNumber = prompt('Size 1-100', 50);
         generate(divNumber);
-        persistent();
-    }
-    //console.log(main);
-    window.onload = generate(divNumber);
-    window.onload = persistent();
+    };
+    document.getElementById("black").onclick = function() { persistent(); };
+    document.getElementById("color").onclick = function() { color(); };
+    document.getElementById("fading").onclick = function() { fading(); };
+    document.getElementById("slowly").onclick = function() { slowly(); };
+    onload = generate(divNumber);
+    onload = persistent();
 })();
