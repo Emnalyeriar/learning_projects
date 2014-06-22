4fab37d0fa6aee1c10660ec0e40ec73816036869
Mateusz <emnalyeriar@gmail.com>
2014-06-22 22:25:03 +0200

    made slides move on an interval with progress bar
    I should definetly clear the code here and there but I upgraded it a
    bit, now the slides move on a set interval and there is a progress bar
    that show the time left unit the slides move

--- jQuery slider v2/slider.js
+++ jQuery slider v2/slider.js
@@ -8,6 +8,7 @@
         this.width = this.imgs[0].width;
         this.length = this.imgs.length;
         this.current = 1;
+        this.speed = 5001;
         var slider = this;
         
         this.container.css({
@@ -74,25 +75,61 @@
                 'height': '20px',
                 'border-radius': '50px',
                 'background-color': '#fff',
+                'opacity': '.5',
+                'transition': 'background-color .5s ease',
                 'top': this.height-50,
                 'left': (this.width/2)-20*(this.length/2)+20*i
             });
         }
         
+        $('<span></span>').insertBefore($('.next')).css({
+            'display': 'inline-block',
+            'position': 'absolute',
+            'width': '0',
+            'height': '5px',
+            'background-color': '#000',
+            'opacity': '.2',
+            'bottom': '5px'
+        });
+        
+        $('.slider span').animate({
+            'width': this.width
+        }, slider.speed);
+        
         $('.slider div:nth-child('+this.current+')').css('background-color', '#000');
         
-        $('.slider a').on('click', function(e){
-            e.preventDefault;
-            ( $(this).text() === '>' ) ? slider.current+=1 : slider.current-=1;
+        this.loop = function() {
+            ( $(this).text() === '<' ) ? slider.current-=1 : slider.current+=1;
             ( slider.current === 0 ) ? slider.current = slider.length : slider.current;
             ( slider. current === slider.length+1 ) ? slider.current = 1 : slider.current;
             $('.slider ul').animate({
                 'margin-left': (slider.current-1)*slider.width*-1
             });
+            $('.slider span').stop();
+            $('.slider span').css({
+                'width': 0
+            });
+            $('.slider span').animate({
+                'width': slider.width
+            }, slider.speed, 'linear');
             $('.slider div').css('background-color', '#fff');
             $('.slider div:nth-child('+slider.current+')').css('background-color', '#000');
+        }
+        
+        var interval = setInterval(function(){
+            slider.loop();
+        }, this.speed);
+        
+        $('.slider a').on('click', function(e){
+            e.preventDefault;
+            clearInterval(interval);
+            slider.loop();
+            interval = setInterval(function(){
+                slider.loop();
+            }, slider.speed);
         });
         
+
     }
     $(window).on('load', function() {
         var slider = new Slider ();
